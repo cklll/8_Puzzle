f81fa22e257dd70cdb500a75ebda6307f7ddc19f
cklll <a418316118abc@gmail.com>
2017-10-06 14:17:55 +0800

    updating IDS

--- BFS.py
+++ BFS.py
@@ -1,84 +1,107 @@
 import queue
-from copy import deepcopy
+import timeit
 
-def solve():
-    state = []
-    state.append(list(map(int,input().split(" "))))
-    state.append(list(map(int,input().split(" "))))
-    state.append(list(map(int,input().split(" "))))
 
-    goal_state = []
-    goal_state.append(list(map(int,input().split(" "))))
-    goal_state.append(list(map(int,input().split(" "))))
-    goal_state.append(list(map(int,input().split(" "))))
+
+def swap_char(s, i, j):
+    l = list(s)
+    l[i], l[j] = l[j], l[i]
+    return ''.join(l)
+
+def print_result(initial_state, space_action):
+    result = ''
+    action = ''
+    current_matrix = initial_state
+    result = 'Initial State:\n' + to_matrix_from_string(current_matrix) + '\n\n'
+    for char in space_action:
+        empty_index = current_matrix.index('0')
+        if char == 'L':
+            action = current_matrix[empty_index - 1] + ' moves right.\n'
+            current_matrix = swap_char(current_matrix, empty_index, empty_index - 1)
+
+        if char == 'R':
+            action = current_matrix[empty_index + 1] + ' moves left.\n'
+            current_matrix = swap_char(current_matrix, empty_index, empty_index + 1)
+
+        if char == 'U':
+            action = current_matrix[empty_index - 3] + ' moves down.\n'
+            current_matrix = swap_char(current_matrix, empty_index, empty_index - 3)
+
+        if char == 'D':
+            action = current_matrix[empty_index + 3] + ' moves up.\n'
+            current_matrix = swap_char(current_matrix, empty_index, empty_index + 3)
+
+        result += action + to_matrix_from_string(current_matrix) + '\n\n'
+
+    result += 'Total moves: {0}'.format(len(space_action))
+    print(result)
+
+def to_matrix_from_string(s):
+    return "{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}".format(s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8])
+
+
+def solve_BFS(initial_state, goal_state):
+    visited = set([initial_state])
 
     stateQueue = queue.Queue()
-    stateQueue.put({'state': state, 'action': '', 'counter': 0})
+    stateQueue.put({'state': initial_state, 'space_action': ''})
 
-    visited = []
-    visited.append(deepcopy(state))
-    counter = 0
     while not stateQueue.empty():
-        counter += 1
-        print(counter)
-        print(stateQueue.qsize())
         current_state = stateQueue.get()
-
         if current_state.get('state') == goal_state:
-            print('Number of moves is {0}.\n{1}'.format(current_state.get('counter'), current_state.get('action')))
+            print_result(initial_state, current_state.get('space_action'))
             break
 
-        space_X, space_Y = None, None
-        for i in range(3):
-            for j in range(3):
-                if current_state.get('state')[i][j] == 0:
-                    space_X = j
-                    space_Y = i
+        empty_index = current_state.get('state').index('0')
 
-        #space moves left
-        if space_X > 0:
-            nextState = deepcopy(current_state.get('state'))
-            newAction = current_state.get('action') + str(nextState[space_Y][space_X - 1]) + " moves right.\n"
-            nextState[space_Y][space_X] = nextState[space_Y][space_X - 1]
-            nextState[space_Y][space_X - 1] = 0
+        # space moves right
+        if not (empty_index == 2 or empty_index == 5 or empty_index == 8):
+            next_state = swap_char(current_state.get('state'), empty_index, empty_index + 1)
             
-            if nextState not in visited:
-                visited.append(nextState)                
-                stateQueue.put({'state': nextState, 'action': newAction, 'counter': current_state.get('counter') + 1})
+            if next_state not in visited:
+                visited.add(next_state)
+                stateQueue.put({'state': next_state, 'space_action': current_state.get('space_action') + 'R'})
 
-        #space moves right
-        if space_X < 2:
-            nextState = deepcopy(current_state.get('state'))
-            newAction = current_state.get('action') + str(nextState[space_Y][space_X + 1]) + " moves left.\n"
-            nextState[space_Y][space_X] = nextState[space_Y][space_X + 1]
-            nextState[space_Y][space_X + 1] = 0
+        # space moves left
+        if not (empty_index == 0 or empty_index == 3 or empty_index == 6):
+            next_state = swap_char(current_state.get('state'), empty_index, empty_index - 1)
             
-            if nextState not in visited:
-                visited.append(nextState)                
-                stateQueue.put({'state': nextState, 'action': newAction, 'counter': current_state.get('counter') + 1})
+            if next_state not in visited:
+                visited.add(next_state)
+                stateQueue.put({'state': next_state, 'space_action': current_state.get('space_action') + 'L'})
 
-        #space moves up
-        if space_Y > 0:
-            nextState = deepcopy(current_state.get('state'))
-            newAction = current_state.get('action') + str(nextState[space_Y - 1][space_X]) + " moves down.\n"
-            nextState[space_Y][space_X] = nextState[space_Y - 1][space_X]
-            nextState[space_Y - 1][space_X] = 0
+        # space moves up
+        if not (empty_index == 0 or empty_index == 1 or empty_index == 2):
+            next_state = swap_char(current_state.get('state'), empty_index, empty_index - 3)
             
-            if nextState not in visited:
-                visited.append(nextState)                
-                stateQueue.put({'state': nextState, 'action': newAction, 'counter': current_state.get('counter') + 1})
-
-        #space moves down
-        if space_Y < 2:
-            nextState = deepcopy(current_state.get('state'))
-            newAction = current_state.get('action') + str(nextState[space_Y + 1][space_X]) + " moves up.\n"
-            nextState[space_Y][space_X] = nextState[space_Y + 1][space_X]
-            nextState[space_Y + 1][space_X] = 0
+            if next_state not in visited:
+                visited.add(next_state)
+                stateQueue.put({'state': next_state, 'space_action': current_state.get('space_action') + 'U'})
+        
+        # space moves down
+        if not (empty_index == 6 or empty_index == 7 or empty_index == 8):
+            next_state = swap_char(current_state.get('state'), empty_index, empty_index + 3)
             
-            if nextState not in visited:
-                visited.append(nextState)                
-                stateQueue.put({'state': nextState, 'action': newAction, 'counter': current_state.get('counter') + 1})
+            if next_state not in visited:
+                visited.add(next_state)
+                stateQueue.put({'state': next_state, 'space_action': current_state.get('space_action') + 'D'})
 
 
 if __name__ == "__main__":
-    solve()
+    start = timeit.default_timer()
+
+    initial_state = ""
+    initial_state += input().replace(" ", "")
+    initial_state += input().replace(" ", "")
+    initial_state += input().replace(" ", "")
+
+    goal_state = ""
+    goal_state += input().replace(" ", "")
+    goal_state += input().replace(" ", "")
+    goal_state += input().replace(" ", "")
+    
+    solve_BFS(initial_state, goal_state)
+
+    stop = timeit.default_timer()
+
+    print('Time used: {0}'.format(stop - start))
--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 ```
 cat input.txt goal.txt | python BFS.py
 ```
-
 #### Result
 ```
 input.txt
@@ -39,4 +38,17 @@ Number of moves is 26.
 5 moves down.
 2 moves right.
 1 moves right.
+```
+
+## Iterative Deepening Search
+```
+cat input.txt goal.txt | python IDS.py
+```
+#### Result
+```
+input.txt
+7 2 4
+5 0 6
+8 3 1
+
 ```
